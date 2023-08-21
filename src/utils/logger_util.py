222c5f4cf707faa6c6bdc99dee5ed0a5de29e8f8
Raghav Wahi <raghavwahi7@gmail.com>
2023-08-21 09:58:06 +0530

    Added a fix to enable logs in stream. If a stream handler is already attached, same will be used.

--- src/utils/logger_util.py
+++ src/utils/logger_util.py
@@ -15,18 +15,32 @@ class Logger:
         os.makedirs(logs_directory, exist_ok=True)
         log_file = f"{logs_directory}/{datetime.now().strftime('%Y_%m_%d.log')}"
         logger_obj = logging.getLogger(identifier)
+        stream_handler_added = False
 
-        for handler in logger_obj.handlers[:]:
+        for handler in logger_obj.handlers:
             if isinstance(handler, logging.FileHandler):
                 logger_obj.removeHandler(handler)
+            if isinstance(handler, logging.StreamHandler):
+                stream_handler_added = True
 
-        file_handler = logging.FileHandler(log_file)
         config_parser = ConfigParser("config")
         configs = config_parser.get_configs("app_configs")
-        formatter = logging.Formatter(configs["log_message_format"], configs["log_time_format"])
-        file_handler.setFormatter(formatter)
-        logger_obj.setLevel(logging.DEBUG)
+        file_formatter = logging.Formatter(configs["log_file_message_format"],
+                                           configs["log_time_format"])
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setFormatter(file_formatter)
         logger_obj.addHandler(file_handler)
+
+        if not stream_handler_added:
+            stream_formatter = logging.Formatter(
+                configs["log_stream_message_format"].format(identifier=identifier),
+                configs["log_time_format"],
+            )
+            stream_handler = logging.StreamHandler()
+            stream_handler.setFormatter(stream_formatter)
+            logger_obj.addHandler(stream_handler)
+
+        logger_obj.setLevel(logging.DEBUG)
         critical_log_libs = ast.literal_eval(configs["critical_log_libs"])
         for lib in critical_log_libs:
             # Log only critical logs for these libraries.
